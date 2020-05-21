452f972ec895652107d27d7540160a4c862b0997
nolastemgarden <nolastemgarden@gmail.com>
2020-05-21 14:46:25 -0500

    App now renders Tabs, not Game

--- src/App.js
+++ src/App.js
@@ -2,12 +2,82 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// My Custom Components
 import Game from './components/Game'
 
+
+// MUI compound components
+import Tabs from './components/Tabs'
+
+// MUI basic components
+import AppBar from '@material-ui/core/AppBar'
+import Typography from '@material-ui/core/Typography'
+
+
+
+
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles({
+    root: {
+        height: '100vh',
+        width: '100vw',
+        boxSizing: 'border-box',
+        backgroundColor: '#BBFFFF',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    container: {
+        border: 'solid red 1px',
+        height: '90vmin',
+        width: '75vmin',
+    },
+    titleArea: {
+        display: 'flex',
+    },
+    title: {
+        fontSize: 'min(max(1.6rem, 6vw), 2.4rem)',
+    },
+    stemGardenLink: {
+
+    },
+    tabArea: {
+        width: 'inherit'
+    },
+    boardArea: {
+        backgroundColor: 'blue',
+        width: '100%',
+        height: '100%'
+    }
+})
+
+
 export default function App() {
-  return (
-    <div className="App">
-       <Game />
-    </div>
-  );
+    const classes = useStyles();
+  
+    return (
+        <div className={classes.root} >
+            <div className={classes.container} >
+                {/* <AppBar position="static">
+                    <Typography className={classes.title} variant="h6" noWrap >
+                        Connect Four
+                    </Typography>
+                </AppBar> */}
+                <Tabs className={classes.tabArea} />
+            </div>
+            
+            
+            
+            {/* <div className={classes.titleArea}>
+                <span className={classes.title}>
+                    Play Connect Four
+                    </span>
+                <span className={classes.stemGardenLink}>
+                    by the Nola Stem Garden
+                    </span>
+            </div> */}
+            
+        {/* <Game /> */}
+        </div>
+    );
 }
