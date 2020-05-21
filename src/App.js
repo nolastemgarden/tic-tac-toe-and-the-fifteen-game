c621721538890ad6bd887f5a1bb32f6f1879f205
nolastemgarden <nolastemgarden@gmail.com>
2020-05-21 08:31:08 -0500

    Replace starter page with <Game>

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Game from './components/Game'
+
+export default function App() {
+  return (
+    <div className="App">
+       <Game />
+    </div>
+  );
+}
