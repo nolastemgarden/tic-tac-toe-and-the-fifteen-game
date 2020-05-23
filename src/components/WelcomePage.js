93dd3b9235b5e24c2c24df17d0610f4e26c72383
nolastemgarden <nolastemgarden@gmail.com>
2020-05-23 17:47:44 -0500

    Create WelcomePage.js

---
+++ src/components/WelcomePage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+// My Components
+
+
+// MUI  components
+import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
+import AppBar from '@material-ui/core/AppBar'
+import Typography from '@material-ui/core/Typography'
+
+
+
+// Custom Styling
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles({
+
+})
+
+
+export default function WelcomePage() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root} >
+            The  Welcome Page
+        </div>
+    );
+}
