00d093f1c5b14a6b201ab1d1bdada680b3e4b205
nolastemgarden <nolastemgarden@gmail.com>
2020-05-25 08:19:06 -0500

    Extract Square component into its own File

--- src/TicTacToe/Board.js
+++ src/TicTacToe/Board.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // My Components
-
+import Square from "./Square";
 
 // MUI  components
 import Paper from '@material-ui/core/Paper';
@@ -11,13 +11,7 @@ import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 // Custom Styling
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
-    boardArea: {
-        backgroundColor: '#bbffff',
-        height: '50vmin',
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-    },
+    
     board: {
         width: '50vmin',
         height: '50vmin',
@@ -26,73 +20,34 @@ const useStyles = makeStyles({
         alignContent: 'center',
         justifyContent: 'center',
     },
-    square: {
-        margin: '1vmin',
-        width: '13vmin',
-        height: '13vmin',
-        boxSizing: 'border-box',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-
-
-    },
-    iconX: {
-        fontSize: '13vmin',
-        
-    },
-    iconO: {
-        fontSize: '10vmin',
-        
-    }
 });
 
 export default function Board(props) {
     const classes = useStyles();
+    
+    const squareValues = props.data;
+    // const handleSquareClick = props.handleSquareClick;
 
-
-    const squares = props.data.map((squareValue) =>
-        <Square 
-            key={squareValue} 
-            value={squareValue} 
-            // onClick={() => this.props.onClick(i)}
-            // onClick={handleSquareClick}    
+    
+    let board = [];
+    for (let id = 0; id < 9; id++) {
+        let newSquare =
+            <Square
+                key={id}
+                id={id}
+                value={squareValues[id]} 
+                handleClick={props.handleSquareClick}
             />
-    );
+        ;
+        board = board.concat(newSquare);
+    }
+    
+    
 
     return (
-        <div className={classes.boardArea}>
-            <div className={classes.board}>
-                {squares}
-            </div>
+        <div className={classes.board}>
+            {board}
         </div>
     )
 }
 
-function Square(props) {
-    const classes = useStyles();
-    let icon;
-    switch (props.value) {
-        case 'x':
-            icon = <ClearIcon className={classes.iconX} />          
-            break;
-        case 'o':
-            icon = <RadioButtonUncheckedIcon className={classes.iconO} />
-            break;
-        case '':
-            icon = null;
-            break;    
-        default:
-            throw console.error("Square passed value not 'x' 'o' or ''");
-            break;
-    }
-    
-    return (
-        <Paper 
-            className={classes.square}
-            // onClick={handleSquareClick()} 
-        >
-            {icon}
-        </Paper>
-    )
-}
