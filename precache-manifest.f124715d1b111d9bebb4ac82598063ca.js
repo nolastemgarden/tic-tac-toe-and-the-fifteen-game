489354dea9c2a7ced42a8a37addd53305e09b009
nolastemgarden <nolastemgarden@gmail.com>
2020-05-27 10:22:53 -0500

    Updates

---
+++ precache-manifest.f124715d1b111d9bebb4ac82598063ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0681ce882ae649ad42e997384823c713",
+    "url": "/tic-tac-toe-and-the-fifteen-game/index.html"
+  },
+  {
+    "revision": "5815daf837eb1b0023b2",
+    "url": "/tic-tac-toe-and-the-fifteen-game/static/css/main.4738d84e.chunk.css"
+  },
+  {
+    "revision": "38cdc3d92a22ea27ce3e",
+    "url": "/tic-tac-toe-and-the-fifteen-game/static/js/2.cce70f39.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tic-tac-toe-and-the-fifteen-game/static/js/2.cce70f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5815daf837eb1b0023b2",
+    "url": "/tic-tac-toe-and-the-fifteen-game/static/js/main.c92513ed.chunk.js"
+  },
+  {
+    "revision": "eda55060871f3d126f33",
+    "url": "/tic-tac-toe-and-the-fifteen-game/static/js/runtime-main.c584e9d6.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/tic-tac-toe-and-the-fifteen-game/static/media/logo.ee7cd8ed.svg"
+  }
+]);
