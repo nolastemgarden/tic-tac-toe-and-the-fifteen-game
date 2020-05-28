6e2bc82fef4faf842719e2193353b7583e8f2375
nolastemgarden <nolastemgarden@gmail.com>
2020-05-28 17:46:38 -0500

    Add first expansion panel
    how is math different from science

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -12,11 +12,36 @@ import MenuIcon from '@material-ui/icons/Menu';
 import AppBar from '@material-ui/core/AppBar'
 import Typography from '@material-ui/core/Typography'
 
-
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 // Custom Styling
 import { makeStyles } from '@material-ui/core/styles';
-const useStyles = makeStyles({
+import { Grid } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    panel: {
+        width: '100%',
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+    // root: {
+    //     width: '100%',
+    // },
+    // heading: {
+    //     fontSize: theme.typography.pxToRem(15),
+    //     flexBasis: '33.33%',
+    //     flexShrink: 0,
+    // },
+    // secondaryHeading: {
+    //     fontSize: theme.typography.pxToRem(15),
+    //     color: theme.palette.text.secondary,
+    // },
+
     root: {
         display: 'flex',
         flexDirection: 'column',
@@ -30,26 +55,125 @@ const useStyles = makeStyles({
         backgroundColor: '#ffffff',
         height: '50vmin',
         width: 'calc(100% - 3vmin)',
-        
+        boxSizing: 'border-box',
+        padding: '0vmin',
     },
-})
+    title: {
+        fontSize: '3.2vmin',
+    },
+    header: {
+        fontSize: '2.8vmin',
+        fontWeight: 'bold'
+    },
+    body: {
+        fontSize: '2vmin',
+    },
+
+}));
 
 
 export default function WelcomePage() {
     const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
+    };
 
     return (
         <div className={classes.root} >
             <Paper className={classes.paper} >
-                <Typography>
-                    What distinguishes Math from Science? 
-                </Typography>
-                <Typography>
-                    In Science, we observe the natural world and come up with "theories" based on <em>evidence</em>.
-                    In Mathematics, we start with abstract ideas and 
-                    
-                    Science uses Mathematics to look for patterns in data to support theories.
-                </Typography>
+               
+                <ExpansionPanel className={classes.panel} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                    <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1bh-content"
+                        id="panel1bh-header"
+                    >
+                        <Typography className={classes.title} >
+                            What distinguishes Math from Science?
+                        </Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Grid container spacing={4}>
+                            <Grid item xs={6}>
+                                <Typography className={classes.header} >
+                                    In Science:
+                                </Typography>
+                                <Typography className={classes.body} >
+                                    We observe the natural world and collect data. We use math to look for 
+                                    patterns in the data and try to come up with <em>theories</em> that explain 
+                                    what we observe.
+                                </Typography>
+                                <Typography className={classes.body} >
+                                    We use evidence to support our theories, but new tools for observing the world 
+                                    can provide us with new evidence that forces dramatic changes in scientific theory.
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography className={classes.header} >
+                                    In Mathematics:
+                                </Typography>
+                                <Typography className={classes.body}>
+                                    We start with abstract ideas like number and shape. 
+                                    We look for things that we can proove <em>must always be true</em>.
+                                    We try to see beneath-the-surface similarities between 
+                                    parts of the real-world that seem unrelated at first.                                
+                                </Typography>
+                                <Typography className={classes.body} >
+                                    Mathematical proof relies on <em>pure logic, not evidence,</em> 
+                                    so once a theorem is mathematically prooven there is no chance of 
+                                    it later being disproven.
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+                <ExpansionPanel className={classes.panel} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                    <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1bh-content"
+                        id="panel1bh-header"
+                    >
+                        <Typography className={classes.title} >
+                            What distinguishes Math from Science?
+                        </Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Grid container spacing={4}>
+                            <Grid item xs={6}>
+                                <Typography className={classes.header} >
+                                    In Science:
+                                </Typography>
+                                <Typography className={classes.body} >
+                                    We observe the natural world and collect data. We use math to look for
+                                    patterns in the data and try to come up with <em>theories</em> that explain
+                                    what we observe.
+                                </Typography>
+                                <Typography className={classes.body} >
+                                    We use evidence to support our theories, but new tools for observing the world
+                                    can provide us with new evidence that forces dramatic changes in scientific theory.
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Typography className={classes.header} >
+                                    In Mathematics:
+                                </Typography>
+                                <Typography className={classes.body}>
+                                    We start with abstract ideas like number and shape.
+                                    We look for things that we can proove <em>must always be true</em>.
+                                    We try to see beneath-the-surface similarities between
+                                    parts of the real-world that seem unrelated at first.
+                                </Typography>
+                                <Typography className={classes.body} >
+                                    Mathematical proof relies on <em>pure logic, not evidence,</em>
+                                    so once a theorem is mathematically prooven there is no chance of
+                                    it later being disproven.
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
             </Paper>
         </div>
     );
