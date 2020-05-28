3c294462173b2cc061666197599a9bdb0ea8874b
nolastemgarden <nolastemgarden@gmail.com>
2020-05-28 14:19:04 -0500

    Begin layout of welcome page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 
 // MUI  components
-import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -17,7 +17,21 @@ import Typography from '@material-ui/core/Typography'
 // Custom Styling
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
-
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        // backgroundColor: '#4AC9FD'
+    },
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'start',
+        backgroundColor: '#ffffff',
+        height: '50vmin',
+        width: 'calc(100% - 3vmin)',
+        
+    },
 })
 
 
@@ -26,7 +40,17 @@ export default function WelcomePage() {
 
     return (
         <div className={classes.root} >
-            The  Welcome Page
+            <Paper className={classes.paper} >
+                <Typography>
+                    What distinguishes Math from Science? 
+                </Typography>
+                <Typography>
+                    In Science, we observe the natural world and come up with "theories" based on <em>evidence</em>.
+                    In Mathematics, we start with abstract ideas and 
+                    
+                    Science uses Mathematics to look for patterns in data to support theories.
+                </Typography>
+            </Paper>
         </div>
     );
 }
