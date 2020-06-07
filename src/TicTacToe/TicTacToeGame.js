6fc79610b5d311b2c1fd63964d75c5f3939342b1
nolastemgarden <nolastemgarden@gmail.com>
2020-06-07 14:53:40 -0500

    Remove unused MUI imports

--- src/TicTacToe/TicTacToeGame.js
+++ src/TicTacToe/TicTacToeGame.js
@@ -6,15 +6,6 @@ import Panel from "./Panel";
 
 
 // MUI  components
-import Link from '@material-ui/core/Link';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuIcon from '@material-ui/icons/Menu';
-import AppBar from '@material-ui/core/AppBar'
-import Typography from '@material-ui/core/Typography'
-
-
 
 // Custom Styling
 import { makeStyles } from '@material-ui/core/styles';
