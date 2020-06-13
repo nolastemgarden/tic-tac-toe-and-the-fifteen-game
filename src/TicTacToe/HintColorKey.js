0ac4bddcccfdd2f2230d15c26ba143cc73e0d598
nolastemgarden <nolastemgarden@gmail.com>
2020-06-13 12:34:25 -0500

    Made it work
    The hint key displays what I want it to but I know I didn't get it done cleanly.  I struggled to get CSS variables into my JSX with makeStyles()

--- src/TicTacToe/HintColorKey.js
+++ src/TicTacToe/HintColorKey.js
@@ -13,24 +13,31 @@ import Fade from '@material-ui/core/Fade';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 
+
+
 // Custom Styling
 import { makeStyles } from '@material-ui/core/styles';
 import { Box, Typography } from '@material-ui/core';
+
+
+// How do I get this CSS var 
+const immediateWin = `var(--immediate-win-bg-color)`;
+console.log(`immediateWin: ${immediateWin}`)
+
+
+
 const useStyles = makeStyles((theme) => ({
     root: {
+        border: 'solid #bbb 0.2px',
+        boxSizing: 'border-box',
         marginTop: '1rem',
-        flexGrow: '2',
         width: '100%',
-        // height: '100%',
-        border: 'solid red 1px',
-        display: 'flex',
-        flexWrap: 'wrap',
-        flexDirection: 'column',
     },
     
     keyItem: {
-        border: 'solid blue 1px',
-        width: '40%',
+        border: 'solid #bbb 0.2px',
+        boxSizing: 'border-box',
+        // width: '40%',
         flexGrow: '2',
         display: 'flex',
         alignItems: 'center',
@@ -38,35 +45,53 @@ const useStyles = makeStyles((theme) => ({
     },
 
     colorTile: {
-        border: 'solid blue 1px',
+        // border: 'solid blue 1px',
         margin: '1%',
+        marginRight: '1rem',
         borderRadius: '15%',
 
-        width: '3.5vmin',
-        height: '3.5vmin',
+        width: '4vmin',
+        height: '4vmin',
     },
 
     immediateWin: {
+        borderRadius: '20%',
         width: '100%',
         height: '100%',
-        backgroundColor: '#00bb00'
+        backgroundColor: '#00BB00',
+        // backgroundColor: immediateWin
     },
     
     doubleAttackCreatingMove: {
+        borderRadius: '20%',
+        width: '100%',
+        height: '100%',
         backgroundColor: '#55bb00'
     },
     forcedWinCreatingMove: {
+        borderRadius: '20%',
+        width: '100%',
+        height: '100%',
         backgroundColor: '#88ee33'
     },
 
     urgentDefensiveMove: {
+        borderRadius: '20%',
+        width: '100%',
+        height: '100%',
         backgroundColor: '#ff6600'
     },
     unavoidableDefeat: {
-        backgroundColor: '#ff4433'
+        borderRadius: '20%',
+        backgroundColor: '#ff4433',
+        width: '100%',
+        height: '100%',
     },
     gameLosingMove: {
-        backgroundColor: '#EEDD11'
+        borderRadius: '20%',
+        backgroundColor: '#EEDD11',
+        width: '100%',
+        height: '100%',
     }
 }));
 
@@ -87,20 +112,61 @@ export default function HintColorKey() {
                     Immediate Win
                 </Typography>
             </Grid>
-            <Grid item xs={6} className={classes.keyItem} >
 
+            <Grid item xs={6} className={classes.keyItem} >
+                <Box className={classes.colorTile}  >
+                    <Box className={classes.unavoidableDefeat}  >
+
+                    </Box>
+                </Box>
+                <Typography variant="h5">
+                    Unavoidable Defeat
+                </Typography>
             </Grid>
-            <Grid item xs={6} className={classes.keyItem} >
 
+            <Grid item xs={6} className={classes.keyItem} >
+                <Box className={classes.colorTile}  >
+                    <Box className={classes.doubleAttackCreatingMove}  >
+
+                    </Box>
+                </Box>
+                <Typography variant="h5">
+                    Double Attack
+                </Typography>
             </Grid>
-            <Grid item xs={6} className={classes.keyItem} >
 
+            <Grid item xs={6} className={classes.keyItem} >
+                <Box className={classes.colorTile}  >
+                    <Box className={classes.urgentDefensiveMove}  >
+
+                    </Box>
+                </Box>
+                <Typography variant="h5">
+                    Urgent Defensive Move
+                </Typography>
             </Grid>
+            
+            
             <Grid item xs={6} className={classes.keyItem} >
+                <Box className={classes.colorTile}  >
+                    <Box className={classes.forcedWinCreatingMove}  >
 
+                    </Box>
+                </Box>
+                <Typography variant="h5">
+                    Winning Attack
+                </Typography>
             </Grid>
+            
             <Grid item xs={6} className={classes.keyItem} >
+                <Box className={classes.colorTile}  >
+                    <Box className={classes.gameLosingMove}  >
 
+                    </Box>
+                </Box>
+                <Typography variant="h5">
+                    Game Losing Mistake
+                </Typography>
             </Grid>
         </Grid>
 
