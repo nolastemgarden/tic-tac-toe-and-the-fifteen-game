3d9db2bd060ce3603809f415407df69d0952bfbe
nolastemgarden <nolastemgarden@gmail.com>
2020-08-28 16:48:59 -0500

    Adjust font and button sizes.

--- src/components/pages/WelcomePage.js
+++ src/components/pages/WelcomePage.js
@@ -57,9 +57,11 @@ const useStyles = makeStyles((theme) => ({
         // textIndent: '1rem',
         padding: '0.5rem',
         paddingBottom: '0.7rem',
+        // fontSize: '0.9rem',
     },
     buttonArea: {
         // border: 'solid red 1px',
+        flex: '1 0 10%',
         display: 'flex',
         marginBottom: '0.7rem',
         width: '100%',
@@ -69,6 +71,12 @@ const useStyles = makeStyles((theme) => ({
     button: {
         margin: 'auto',
         width: '37%',
+        backgroundColor: theme.palette.primary.dark,
+        textAlign: 'center'
+    }, 
+    wideButton: {
+        margin: 'auto',
+        width: '80%',
         backgroundColor: theme.palette.primary.dark,
         textAlign: 'center'
     }, 
@@ -85,23 +93,20 @@ export default function WelcomePage() {
         <Box className={classes.root} >
             <Paper className={classes.paper} elevation={5} >
                 <Typography className={classes.title} gutterBottom>
-                    Welcome to Tic-Tac-Toe and the 15-Game!
+                    Tic-Tac-Toe & The 15-Game!
                 </Typography>
-                <Typography variant='body1' className={classes.paragraph}>
-                    My name is Nigel, and I am a math teacher who loves games where simple sets of rules open a door to complex strategic possibilities.
-                    I love to use games as a lens through which to teach math beacuse they remind us that, though math is based on disciplined reasoning, 
-                    it has a playful side which feeds the desire to explore.
+                <Typography variant='body2' className={classes.paragraph}>
+                    My name is Nigel, and I am a math teacher who loves games where a few easy to learn rules open a door to complex strategies.
+                    I love to use games to teach math beacuse games remind us that, though math is based on precise and disciplined reasoning, 
+                    it has a playful side too! Keeping in touch with this playful side feeds a mathematician's desire to learn.
                 </Typography>
-                <Typography variant='body1' className={classes.paragraph}>
-                    In "How Not To Be Wrong", Jordan Elenberg explains, "Knowing mathematics is like wearing a pair of X-ray
+                <Typography variant='body2' className={classes.paragraph}>
+                    In "How Not To Be Wrong: The power of mathematical thinking", Jordan Elenberg explains, "Knowing mathematics is like wearing a pair of X-ray
                     specs that reveal hidden structures beneath the messy and chotic surface of the world."  When two real-world
                     problems that seem unrelated on their surfaces' are actually behaving according
                     to the same underlying patterns, Math helps us become aware of that fact.  But don't take my word for it!
-                </Typography>
-                <Typography variant='body1' className={classes.paragraph}>
-                    To see this for yourself, play Tic-Tac-Toe and then play the Fifteen Game. You will see that (on the surface) these two
-                    games feel quite different to play.  You would not likely suspect that knowing the best strategies in Tic-Tac-Toe
-                    would make you a stronger player at the Fifteen Game, too.
+                    To see this for yourself, play Tic-Tac-Toe and then play the Fifteen Game. You will see that these two
+                    games feel quite different to play. 
                 </Typography>
                 <Box className={classes.buttonArea}>
                     <Button
@@ -111,7 +116,7 @@ export default function WelcomePage() {
                         component={RouterLink}
                         to='/tic-tac-toe'
                     >
-                        Play Tic-Tac-Toe
+                        Play<br/>Tic-Tac-Toe
                     </Button>
 
                     <Button
@@ -121,26 +126,26 @@ export default function WelcomePage() {
                         component={RouterLink}
                         to='/fifteen-game'
                     >
-                        Play the 15-Game
+                        Play the<br />15-Game
                     </Button>
                 </Box>
-                <Typography variant='body1' className={classes.paragraph}>
+                <Typography variant='body2' className={classes.paragraph}>
                     When you have played both games check out the lesson on Magic Squares. Magic Squares are a mathematical curiosity
-                    that were first studied in China and India thousands of years ago. Once you know about magic squares you will be
-                    able to see that under the hood Tic-Tac-Toe and the Fifteen Game are actually the same game! Master one and you will have mastered both!
+                    that were first studied in China and India thousands of years ago. Once you know more about magic squares you will be
+                    able to see that, beneath the surface, Tic-Tac-Toe and the Fifteen Game are actually the same game! Master one and you will have mastered both!
                 </Typography>
                 <Box className={classes.buttonArea}>
                     <Button
-                        className={classes.button}
+                        className={classes.wideButton}
                         variant="contained"
                         color="primary"
                         component={RouterLink}
                         to='/magic-squares'
                     >
-                        Learn Magic Squares
+                        Learn About Magic Squares
                     </Button>
 
-                    <Button
+                    {/* <Button
                         className={classes.button}
                         variant="contained"
                         color="primary"
@@ -148,9 +153,9 @@ export default function WelcomePage() {
                         to='/strategy'
                     >
                         Master the Strategy
-                    </Button>
+                    </Button> */}
                 </Box>
-                <Typography variant='body1' className={classes.paragraph}>
+                <Typography variant='body2' className={classes.paragraph}>
                     This lesson is built with code that is open-source and available on my <a href="https://github.com/nolastemgarden">Github account</a>,
                 so if you have something to add or simply want to see how it works you are welcome to &nbsp;
                 <a href="https://github.com/nolastemgarden/tic-tac-toe-and-the-fifteen-game">clone the repository</a>!
