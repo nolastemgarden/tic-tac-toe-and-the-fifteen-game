19630a07f9c6c585ffe0cec7f5a686a6f38087d3
nolastemgarden <nolastemgarden@gmail.com>
2020-12-19 00:50:05 -0600

    Cleanup

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -20,60 +20,43 @@ import Box from '@material-ui/core/Box';
 import Grid from "@material-ui/core/Grid";
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
-// import Button from '@material-ui/core/Button';
-// import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-// import Grow from '@material-ui/core/Grow';
-// import Paper from '@material-ui/core/Paper';
-// import Popper from '@material-ui/core/Popper';
-// import MenuList from '@material-ui/core/MenuList';
-// import Menu from '@material-ui/core/Menu';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import MenuIcon from '@material-ui/icons/Menu';
-// import Link from '@material-ui/core/Link';
-// import List from '@material-ui/core/List';
-// import ListItem from '@material-ui/core/ListItem';
-// import ListItemText from '@material-ui/core/ListItemText';
-// import Icon from '@material-ui/core/Icon';
+
 
 
 import { makeStyles } from '@material-ui/core/styles';
+import { CallReceived } from '@material-ui/icons';
+
+const heightInRem = 3.6;
+const height = `${heightInRem}rem`;
+const halfHeight = `${heightInRem/2}rem`;
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        backgroundColor: theme.palette.primary.main,
-        zIndex: '1200',
-    },
-    paper: {
-        marginRight: theme.spacing(2),
-    },
-    navbar: {
+    navbarRoot: {
+        // border: 'solid blue 1px',
         width: '100vw',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        backgroundColor: theme.palette.primary.main, 
-        display: 'flex',
+        height: height,
+
+        backgroundColor: theme.palette.primary.main,
         color: theme.palette.common.white,
-    },
-    oneSizeNavbar: {
-        height: '4.0rem',
-    },
-    
 
+        zIndex: '1200',
+        display: 'flex',
+
+    },
     navbarContainer: {
-        height: '100%',
-        backgroundColor: theme.palette.primary.main, 
+        // border: 'solid blue 1px',
+        height: height,
+        backgroundColor: 'inherit',
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-between',
-        // alignItems: 'flex-end'
-
     },
-
+    spacerBox: {
+        // border: 'solid blue 1px',
+        padding: halfHeight,
+    },
     logo: {
-        // border: 'solid red 1px',
-        height: '100%',
+        height: height,
+        width: height,
         backgroundImage: `
             url(${logo})  
         `,
@@ -81,70 +64,33 @@ const useStyles = makeStyles((theme) => ({
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'auto 95%',
     },
-    // mobileBrand: {
-    //     height: '100%',
-
-    //     // border: 'solid blue 1px',
-    //     // paddingTop: '0.4rem',
-    //     // paddingBottom: '0.1rem',
-    //     flex: '2 0 60%',
-    //     display: 'flex',
-    //     // flexDirection: 'column',
-    //     justifyContent: 'center',
-    //     alignItems: 'center'
-    // },
     pageTitle: {
         // border: 'solid red 1px',
         height: '100%',
+        flex: '2 1 80%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
     },
     
-    
-    desktopMenuButton: {
-        color: theme.palette.common.white,
-        textTransform: "none",
-        textAlign: 'center',
-        paddingBottom: '0.5rem',
-    },
-    desktopMenuDropDown: {
-        border: 'solid white 0.1px',
-        backgroundColor: theme.palette.primary.main, // Matches Logo Background
-    },
-
-    listItem: {
-        color: theme.palette.common.white,
-    },
     icon: {
         width: '45px',
         color: theme.palette.common.white,
         alignContent: 'center',
         justifyContent: 'center',
     },
-    spacerBox: {
-        // border: 'solid red 1px',
-        padding: '1.90rem'
-    },
+    
 }));
-
-
 
 
 export default function Navbar(props) {
     const classes = useStyles();
     return (
-        <Box className={classes.root}  >
+        <Box className={classes.navbarRoot} >
             <OneSizeNavbar pageTitle={props.pageTitle} />
-            <Box className={classes.spacerBox}
-                display={{ xs: 'block' }}
-            />
+            <Box className={classes.spacerBox} />
         </Box>
-        // <Box className={classes.root}  >
-        //     <MobileNavbar pageTitle={props.pageTitle} />
-        //     <DesktopNavbar pageTitle={props.pageTitle} />
-        //     <SpacerBox />
-        // </Box>
+        
     )
 }
 
@@ -155,50 +101,7 @@ function OneSizeNavbar(props) {
     return (
         <Box  >
             <AppBar
-                className={classes.navbar, classes.oneSizeNavbar}
-                position="fixed"
-                elevation={3}
-            >
-                <Container
-                    className={classes.navbarContainer}
-                    maxWidth='md'
-                    disableGutters
-                >
-                    <Grid container>
-                        <Grid item xs={2} >
-                            <Box className={classes.logo} />
-                        </Grid>
-                        <Grid item xs={8} >
-                            <Typography className={classes.pageTitle} align='center' variant='h4' >
-                                {pageTitle}
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={2} >
-                            <MobileMenu />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </AppBar>
-
-            {/* <AppBar
-                className={classes.navbar, classes.mobileNavbar}
-                
-            ></AppBar> */}
-
-
-        </Box>
-    );
-}
-
-
-function MobileNavbar(props) {
-    const classes = useStyles();
-    const pageTitle = props.pageTitle;
-
-    return (
-        <Box display={{ xs: 'block', md: 'none' }} >
-            <AppBar
-                className={classes.navbar, classes.mobileNavbar}
+                className={classes.navbar}
                 position="fixed"
                 elevation={3}
             >
@@ -225,75 +128,4 @@ function MobileNavbar(props) {
         </Box>
     );
 }
-
-
-
-
-function DesktopNavbar(props) {
-    const classes = useStyles();
-    const pageTitle = props.pageTitle;
-
-    return (
-        <Box display={{ xs: 'none', md: 'block' }} >
-            <AppBar
-                className={classes.navbar, classes.desktopNavbar}
-                position="fixed"
-                elevation={3}
-            >
-                <Container
-                    className={classes.navbarContainer}
-                    maxWidth='md'
-                    disableGutters
-                >
-                    <Grid container>
-                        <Grid item xs={2} >
-                            <Box className={classes.logo} />
-                        </Grid>
-                        <Grid item xs={8} >
-                            <Typography className={classes.pageTitle} align='center' variant='h4' >
-                                {pageTitle}
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={2} >
-                            <DesktopMenu />
-                        </Grid>
-                    </Grid>
-                    {/* <Box className={classes.logo} />
-                    <Box className={classes.desktopBrand} >
-                        STEM <br />Garden
-                    </Box>
-
-                    <Box className={classes.desktopMenuButtons} >
-                        
-                        <Button
-                            className={classes.button}
-                            variant="outlined"
-                            color="primary"
-                            component={RouterLink}
-                            to='/tic-tac-toe'
-                        >
-                            Play<br />Tic-Tac-Toe
-                        </Button>
-
-                        <Button
-                            className={classes.button}
-                            variant="outlined"
-                            color="primary"
-                            component={RouterLink}
-                            to='/fifteen-game'
-                        >
-                            Play the<br />15-Game
-                        </Button> */}
-                    {/* </Box> */}
-                    
-                    
-                </Container>
-            </AppBar>
-            
-        </Box>
-    );
-}
-
-
-
 
