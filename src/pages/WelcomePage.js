98439b7d12de4ba5d449914652ffa904560fca83
nolastemgarden <nolastemgarden@gmail.com>
2020-12-22 13:30:18 -0600

    Update WelcomePage.js

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -20,8 +20,6 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     
     root: {
-        border: 'solid red 1px',
-
         width: '100%',
         height: 'auto',
         overflow: 'scroll',
@@ -30,31 +28,17 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         flexDirection: 'column',
         color: theme.palette.common.white,
-        padding: '1.0rem 0.0rem',
+        padding: '1.0rem',
         
     }, 
     
-    buttonArea: {
-        // border: 'solid red 1px',
-        flex: '1 0 10%',
-        display: 'flex',
-        marginBottom: '0.7rem',
-        width: '100%',
-        padding: '0.5rem',
-
-    },
     button: {
-        margin: 'auto',
-        // width: '37%',
+        // margin: 'auto',
         backgroundColor: theme.palette.primary.main,
-        textAlign: 'center'
+        textAlign: 'center',
+        marginRight: '1.0rem',
     }, 
-    wideButton: {
-        margin: 'auto',
-        width: '80%',
-        backgroundColor: theme.palette.primary.main,
-        textAlign: 'center'
-    }, 
+    
     
 }));
 
@@ -72,31 +56,41 @@ export default function WelcomePage() {
                 A math lesson taught through two classic games
             </Typography> */}
             <Grid container>
-                <Grid item xs={12} md={8} >
-                    <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
-                        Tic-Tac-Toe and Mathematical Proof
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} md={4} >
+                <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
+                    Intro to Proof
+                </Typography>
+                <Typography paragraph align='justify' color='secondary' component='body1' variant='body1' gutterBottom >
+                    Math is all about things that can be proven using logic.
+                    In the same way a song is an expression of a musician's creativity, a written proof is an expression of a mathematician's creativity.
+                    To the trained eye, mathematical proofs can be profoundly beautiful and elegant,
+                    however, they often involve a lot of abstraction and formality, making them inaccessible to beginners.
+                    The fist goal of this lesson is to break that barrier and introduce the concept of proof in a way 
+                    that is accessible to young students and to beginners with little formal math training.
+                    In this lesson you will prove some interseting things about Tic-Tac-Toe,
+                    without relying on any mathematical jargon or algebra.
+                </Typography>
+                <Box mb={2} > 
                     <Button
                         className={classes.button}
                         variant="contained"
                         color="primary"
                         component={RouterLink}
-                        to='/tic_tac_toe'
+                        to='/play_tic_tac_toe'
                     >
-                        Play  Tic-Tac-Toe!
+                        Play Tic-Tac-Toe!
                     </Button>
-                </Grid>
-                <Typography paragraph align='justify' color='secondary' component='body1' variant='body1' gutterBottom >
-                    Math is all about things that can be proven using logic.
-                    In the same way a song is an expression of a musician's creativity, a written proof is an expression of a mathematician's creativity.
-                    To the trained eye, mathematical proofs can be stunningly beautiful and elegant.
-                    However, they often involve a lot of abstraction and formality, making them inaccessible to beginners.
-                    This lesson introduces students to the concept of proof without depending on students knowing a lot of mathematical jargon or
-                    having prior experience with algebra.
-                </Typography>
-                <Typography paragraph align='justify' color='secondary' component='body1' variant='body1' gutterBottom >
+                    <Button
+                        className={classes.button}
+                        variant="contained"
+                        color="primary"
+                        component={RouterLink}
+                        to='/learn_tic_tac_toe'
+                    >
+                        Learn Tic-Tac-Toe
+                    </Button>
+                </Box>
+                
+                {/* <Typography paragraph align='justify' color='secondary' component='body1' variant='body1' gutterBottom >
                     If you have played Tic-Tac-Toe much, you have probably observed that the player who goes first seems to have an advantage but the game usually ends in a draw.
                     Try and take this casual observation to the next level by prooving that the game will <em>always</em> end in a draw unless one of the players makes a mistake.
                     To do this you will need to articulate a move-by-move plan that you can use when you go second that anticipates
@@ -106,14 +100,31 @@ export default function WelcomePage() {
                     but when you have perfected your plan you won't make any mistakes either and you will never lose to my bot.
                     When you have perfected your strategy for going second try this challenge question: Is it possible for the player who goes first
                     to make a mistake on the first move of the game?
-                </Typography>
+                </Typography> */}
                 
-                <Grid item xs={12} md={8} >
-                    <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
-                        Similarity Beneath the Surface
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} md={4} >
+                <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
+                    Similarity Beneath the Surface
+                </Typography>
+                   
+                <Typography paragraph align='justify' color='secondary' component='body1' variant='body1' >
+                    Many problems can be solved with less work if, instead of starting from scratch, you start
+                    with the solution to a different problem in mind and adapt that solution to the problem at hand. 
+                    The first step in this process is realizing that the two problems have something in common. 
+                    Often, two real-world problems will seem unrelated on their surfaces even though at a deeper 
+                    level they are behaving according to the same patterns. Being familiar with mathematics 
+                    makes you better at noticing when this is the case.
+                </Typography>
+                <Typography paragraph align='justify' color='secondary' component='body1' variant='body1' >
+                    The second goal of this lesson is to give students an oportunity to experience this adaptive process for themselves.
+                    Once you have mastered Tic-Tac-Toe and written up a strategic plan that lets you guarantee you cannot be beat, 
+                    try playing the Fifteen Game. 
+                    At first, you will probably think that these two games feel nothing alike.
+                    You would never guess that by mastering Tic-Tac-Toe you had gotten any closer to mastering the Fifteen Game, but you have!
+                    Once you are familiar with the mechanics of the Fifteen Game, take a look at the lesson on Magic Squares, a mathematical 
+                    curiosity first discovered in China about 4200 years ago. Equipped with this mathematical knowledge, you will soon realize 
+                    that Tic-Tac-Toe and the Fifteen Game have a lot more in common that you thought!
+                </Typography>
+                <Box mb={2} >
                     <Button
                         className={classes.button}
                         variant="contained"
@@ -123,27 +134,7 @@ export default function WelcomePage() {
                     >
                         Play  the 15-Game!
                     </Button>
-                </Grid>    
-                <Typography paragraph align='justify' color='secondary' component='body1' variant='body1' >
-                    Many problems can be solved with less work if you start out with the solution to a different problem in mind and adapt that 
-                    solution to the problem at hand. The first step in being able to save work by reusing a solution is realizing that the two 
-                    problems have something in common. Often this is quite difficult. Two real-world problems may seem totally unrelated on their 
-                    surfaces even though at a deeper level they are behaving according to the same patterns.
-                    Being familiar with common mathematical patterns makes you better at noticing when this is the case.
-                </Typography>
-                <Typography paragraph align='justify' color='secondary' component='body1' variant='body1' >
-                    My second goal in creating this lesson was to get students to experience for themselves a situation where they are
-                    able to adapt and reuse their own solution to another problem. Once you have mastered Tic-Tac-Toe and written a complete 
-                    strategy that lets you guarantee you will never lose, try playing the Fifteen Game. 
-                    At first, you will probably think that these two games feel totally unalike.
-                    You would never guess that by mastering Tic-Tac-Toe you had gotten any closer to mastering the Fifteen Game, but you have!
-                    Once you are familiar with the mechanics of the Fifteen Game, take a look at the lesson on Magic Squares, a mathematical 
-                    curiosity first discovered in China about 4200 years ago. Equipped with this mathematical knowledge, you will soon realize 
-                    that Tic-Tac-Toe and the Fifteen Game have a lot more in common that you thought!
-                </Typography>
 
-                <Grid item xs={12} md={8} / >
-                <Grid item xs={12} md={4} >
                     <Button
                         className={classes.button}
                         variant="contained"
@@ -153,7 +144,8 @@ export default function WelcomePage() {
                     >
                         Learn About Magic Squares
                     </Button>
-                </Grid>    
+                </Box>
+                
                 
                 <Typography variant='body2' className={classes.paragraph}>
                     This lesson is built with code that is open-source and available on my <a href="https://github.com/nolastemgarden">Github account</a>,
